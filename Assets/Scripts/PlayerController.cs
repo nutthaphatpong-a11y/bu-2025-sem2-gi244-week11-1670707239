2de6ee375e95cdfac2438d84096904e4afa68b65
Kita22-Mahou <110737067+Kita22-Mahou@users.noreply.github.com>
2026-03-27 16:46:41 +0700

    finjal

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,12 +5,14 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
-
+    public Transform FocalPoint;
     private Rigidbody rb;
 
     private InputAction moveAction;
     private InputAction smashAction;
     private InputAction breakAction;
+
+    public bool havePowerUp = false;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -25,6 +27,48 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var move = moveAction.ReadValue<Vector2>();
+        rb.AddForce(move.y * speed * FocalPoint.forward);
 
+        if (breakAction.IsPressed())
+        {
+            rb.linearVelocity = Vector3.zero;
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy")) 
+        {
+            if (havePowerUp == true) {
+                var rb = collision.gameObject.GetComponent<Rigidbody>();
+                var dir = collision.transform.position - transform.position;
+                dir.Normalize();
+                rb.AddForce(100 * dir,ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("PowerUp"))
+        {
+            havePowerUp = true;
+            Destroy(other.gameObject);
+            if (countDownCoroutine != null) 
+            {
+            StopCoroutine(countDownCoroutine);
+            }
+            countDownCoroutine =  StartCoroutine(PowerUpCountDown());
+        }
+
+    }
+
+    private Coroutine countDownCoroutine;
+    IEnumerator PowerUpCountDown()
+    {
+        yield return new WaitForSeconds(10f);
+        havePowerUp = false;
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,13 +1,73 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SpawnManager : MonoBehaviour
 {
     public Transform[] spawnPoints;
     public GameObject enemyPrefab;
+    public Rigidbody box;
 
     void Start()
     {
+        //InvokeRepeating(nameof(RandomSpawn), 1f, 2f);
+        //StartCoroutine(Hello());
+        //StartCoroutine(Goodbye());
+        StartCoroutine(SpawnRoutine());
 
     }
 
-}
+    private void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        yield return new WaitForSeconds(5);
+        while (true)
+        {
+            RandomSpawn();
+            yield return new WaitForSeconds(3);
+        }
+    }
+
+    void RandomSpawn()
+    {
+        int Index = Random.Range(0, spawnPoints.Length);
+        var spawnPoint = spawnPoints[Index];
+        Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity);
+    }
+
+
+        IEnumerator Hello()
+        {
+            Debug.Log("Hello " + Time.frameCount);
+            yield return null;
+        }
+
+        IEnumerator Goodbye()
+        {
+            while (true)
+            {
+
+                Debug.Log("HGoodBye " + Time.frameCount + " " + Time.time);
+                //yield return new WaitForSeconds(1);
+                yield return null;
+                yield break;
+                //StartCoroutine(Hello());               
+            }
+        }
+        IEnumerator Movebox()
+        {
+            box.linearVelocity = 10 * Vector3.up;
+            yield return new WaitForSeconds(3);
+            box.linearVelocity = 10 * Vector3.right;
+            yield return new WaitForSeconds(3);
+            box.linearVelocity = 10 * Vector3.down;
+            yield return new WaitForSeconds(3);
+            box.linearVelocity = 10 * Vector3.left;
+            yield return new WaitForSeconds(3);
+        }
+    }
+
